f5be4bfb95bb5eae6084ae89890d5471498e38b1
noemi06m <noemimazzali06@gmail.com>
2025-05-07 13:18:17 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 8080;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/primapag.html");
+});
+
+app.get("/pag2", (req, res) => {
+    res.sendFile(__dirname + "/pag2.html");
+});
+
+app.post("/submit", (req, res) => {
+    const userInput = req.body.text; 
+    res.send(`<h1>il tuo nome e': ${userInput}</h1><br><a href="/">Torna</a>`);
+});
+
+
+app.listen(port, () => {
+    console.log(`Server in ascolto su http://localhost:${port}`);
+});
